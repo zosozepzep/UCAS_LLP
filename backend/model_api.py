8a652101d1b6c95e8fc3ba691d2fc4c695ca4ebf
zoso <3473782389@qq.com>
2026-04-12 20:55:17 +0800

    feat:模型组新增demo

---
+++ backend/model_api.py
@@ -0,0 +1,19 @@
+import requests
+def generate(prompt):
+    url = "http://localhost:11434/api/generate"
+    data = {
+        "model": "gemma4:e2b",
+        "prompt": prompt,
+        "stream": False,
+        "options": {
+            "temperature": 0.7,
+            "top_p": 0.9,
+            "top_k": 40,
+            "repeat_penalty": 1.1,
+            "num_predict": 1000,
+        }
+    }
+    res = requests.post(url, json=data)
+    return res.json()["response"]
+#example
+print(generate("what is AI?"))
